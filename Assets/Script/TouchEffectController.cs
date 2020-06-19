5735c4cac2038de87df9cbb218207ef255fd9923
HoGyeomNa <zzx0147@gmail.com>
2020-06-20 04:00:12 +0900

    add new Background

---
+++ Assets/Script/TouchEffectController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class TouchEffectController : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
