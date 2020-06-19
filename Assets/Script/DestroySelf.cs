23ce13a1b254bd7d17fd1c4295a2560de7badbef
HoGyeomNa <zzx0147@gmail.com>
2020-06-20 08:48:43 +0900

    add Normal Touch and Plant Touch

---
+++ Assets/Script/DestroySelf.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DestoryCountDown());
+    }
+
+    IEnumerator DestoryCountDown()
+    {
+        yield return new WaitForSecondsRealtime(0.5f);
+        Destroy(gameObject);
+    }
+}
--- Assets/Script/TouchEffectController.cs
+++ Assets/Script/TouchEffectController.cs
@@ -1,14 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
 public class TouchEffectController : MonoBehaviour
 {
+    [SerializeField] private GameObject m_NormalTouchEffectPrefab;
+    [SerializeField] private GameObject m_PlantTouchEffectPrefab;
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
+        RaycastWorldUI();
+    }
 
+    void RaycastWorldUI()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PointerEventData pointerData = new PointerEventData(EventSystem.current);
+
+            pointerData.position = Input.mousePosition;
+
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerData, results);
+
+            if (results.Count > 0)
+            {
+                //WorldUI is my layer name
+                if (results[0].gameObject.layer == LayerMask.NameToLayer("UI"))
+                {
+                    if (results[0].gameObject.GetComponent<Plant>() != null)
+                    {
+                        Instantiate(m_PlantTouchEffectPrefab, results[0].worldPosition + new Vector3(0, 0, 0.5f), Quaternion.Euler(0, 0, 0)).transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+                    }
+                    else
+                    {
+                        Instantiate(m_NormalTouchEffectPrefab, results[0].worldPosition + new Vector3(0, 0, 0.5f), Quaternion.Euler(0, 0, 0)).transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+                    }
+                    //string dbg = "Root Element: {0} \n GrandChild Element: {1}";
+                    //Debug.Log(string.Format(dbg, results[results.Count - 1].gameObject.name, results[0].gameObject.name));
+                    ////Debug.Log("Root Element: "+results[results.Count-1].gameObject.name);
+                    //Debug.Log("GrandChild Element: "+results[0].gameObject.name);
+                    results.Clear();
+                }
+            }
         }
     }
 }
