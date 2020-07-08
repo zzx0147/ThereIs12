4870a7bf61a779a240825e21a7a482db69b6cec7
HoGyeomNa <zzx0147@gmail.com>
2020-07-08 12:19:31 +0900

    add FPS Counter

---
+++ Assets/Script/FPSShower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSShower : MonoBehaviour
+{
+    [SerializeField] Text m_text;
+
+    private void Start()
+    {
+        StartCoroutine(UpdateFPS());
+    }
+
+    IEnumerator UpdateFPS()
+    {
+        while(true)
+        {
+            m_text.text = ((int)(1 / Time.unscaledDeltaTime)).ToString();
+            yield return new WaitForSecondsRealtime(0.1f);
+        }
+    }
+    
+}
