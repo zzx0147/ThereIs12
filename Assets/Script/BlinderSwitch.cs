855b145f8f2c7d5dca83b6eed0164a41cd6f564d
HoGyeomNa <zzx0147@gmail.com>
2020-07-10 00:05:23 +0900

    add PopupPanel with ExitOnTouchExternal and update csv

---
+++ Assets/Script/BlinderSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinderSwitch : MonoBehaviour
+{
+    [SerializeField] GameObject m_Target;
+
+    private void OnEnable()
+    {
+        if (m_Target != null)
+        {
+            m_Target.SetActive(true);
+            m_Target.GetComponent<Button>().onClick.AddListener(() => gameObject.SetActive(false));
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (m_Target != null)
+        {
+            m_Target.SetActive(false);
+            m_Target.GetComponent<Button>().onClick.RemoveAllListeners();
+        }
+    }
+}
